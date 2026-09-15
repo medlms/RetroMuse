098945079743709ee9238d251711ca9993efdd64
Mohamed Lemsafi <lemsafim@gmail.com>
2026-09-15 12:12:16 +0200

    Bump versionCode to 17 (version 1.0.15)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,8 +25,8 @@ android {
         applicationId = "com.retro.retromuse"
         minSdk = 26
         targetSdk = 36
-        versionCode = 16
-        versionName = "1.0.14"
+        versionCode = 17
+        versionName = "1.0.15"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
